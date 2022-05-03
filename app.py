0cbbc648dfe8ffff09aa2a358d6d82969d444a13
IrinaSing <irinasingh.mail@yandex.ru>
2022-05-03 16:30:32 +0200

    flask server setup

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 # code for your API
+from ctypes import util
+from urllib import response
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/location_names')
+def get_location_names():
+    response = jsonify({
+        "locations": util.get_location_names
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("start flask server!!!")
+    app.run()
