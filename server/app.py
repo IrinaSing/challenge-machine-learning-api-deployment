83c2a4310aa39b1b06e39ef606039b967c4424ed
IrinaSing <irinasingh.mail@yandex.ru>
2022-05-05 09:02:16 +0200

    app and html

--- server/app.py
+++ server/app.py
@@ -1,20 +1,46 @@
 # code for your API
-from ctypes import util
+#from ctypes import util
 from urllib import response
 from flask import Flask, request, jsonify
-import server.util as util
+import util
 
 app = Flask(__name__)
 
-@app.route('/location_names')
+@app.route('/hello')
+def hello():
+    return "hi"
+
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
-        "locations": util.get_location_names
+        'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
+@app.route('/predict_house_price', methods=['POST'])
+def predict_house_price():
+    location = request.form['location']
+    number_of_bedrooms = int(request.form['number_of_bedrooms'])
+    living_area = float(request.form['living_area'])
+    furnished = bool(request.form['furnished'])
+    open_fireplace = bool(request.form['open_fireplace'])
+    terrace = bool(request.form['terrace'])
+    garden = bool(request.form['garden'])
+    surface_area_land = float(request.form['surface_area_land'])
+    pool = bool(request.form['pool'])
+    condition = request.form['condition']
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,number_of_bedrooms, living_area, furnished, open_fireplace, terrace, garden, surface_area_land,pool, condition)
+    })
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
 if __name__ == "__main__":
-    print("start flask server!!!")
+    print("start flask server")
+    util.load_saved_artifacts()
     app.run()
--- server/util.py
+++ server/util.py
@@ -1,9 +1,46 @@
-from copyreg import pickle
+import pickle
 import json
+import numpy as np
 
+__features = None
 __locations = None
-__data_columns = None
+__condition = None
 __model = None
+
+
+
+
+def get_estimated_price(location,number_of_bedrooms, living_area, furnished, open_fireplace, terrace, garden, surface_area_land,pool,condition):
+    try:
+        loc_index = __locations.index(location.lower())
+    except:
+        loc_index = -1
+
+    try:
+        cond_index = __condition.index(condition.lower())
+    except:
+        cond_index = -1
+
+    x = np.zeros(len(__features) + len(__condition) + len(__locations))
+    x[0] = number_of_bedrooms
+    x[1] = living_area
+    x[2] = furnished
+    x[3] = open_fireplace
+    x[4] = terrace
+    x[5] = garden
+    x[6] = surface_area_land
+    x[7] = pool
+
+    if loc_index>=0:
+        x[loc_index] = 1
+
+    if cond_index>=0:
+        x[cond_index] = 1
+    
+    return round(__model.predict([x])[0],2)
+
+
+
 
 def get_location_names():
     return __locations
@@ -11,18 +48,26 @@ def get_location_names():
 
 def load_saved_artifacts():
     print("loading saved artifacts")
-    global __data_columns
+    
     global __locations
+    global __features
+    global __condition
 
     with open("./artifacts/columns.json", 'r') as f:
-        __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[7:]
+        json_loaded = json.load(f)
+        __locations = json_loaded['locations']
+        __features = json_loaded['features']
+        __condition = json_loaded['condition']
 
-    with open("./artifacts/model_lg.pickle", 'rb') as m:
-        __model = pickle.load(m)
-        print("loading the artifacts is done")
+    global __model
+    if __model is None:
+        with open('./artifacts/model_lg.pkl', 'rb') as f:
+            __model = pickle.load(f)
+        print("loading saved artifacts...done")
 
 
+    
 if __name__ == '__main__':
     load_saved_artifacts()
-    print(get_location_names())
+    #print(get_estimated_price('Gent', 3, 200, False, False, True, True, 200, False, 'as_new'))
+    #print(get_estimated_price('Gent', 3, 300, True, False, False, True, 100, False, 'to_renovate'))
